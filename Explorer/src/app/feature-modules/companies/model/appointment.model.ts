809e0d1c9e7ff4e585f94db6bdc55e537d76548f
Jelena Kovač <jelena.kovac765@gmail.com>
2023-12-16 14:12:09 +0100

    fix: displaying admin name in predefined appointments

--- Explorer/src/app/feature-modules/companies/model/appointment.model.ts
+++ Explorer/src/app/feature-modules/companies/model/appointment.model.ts
@@ -1,27 +1,28 @@
-import { User } from "src/app/infrastructure/auth/model/user.model";
-import { Equipment } from "../../administration/model/equipment.model";
-import { Company } from "./company.model";
-import { RegisteredUser } from "../../users/model/registered-user.model";
+import { User } from 'src/app/infrastructure/auth/model/user.model';
+import { Equipment } from '../../administration/model/equipment.model';
+import { Company } from './company.model';
+import { RegisteredUser } from '../../users/model/registered-user.model';
 
 export interface Appointment {
-    id?: number;
-    pickupTime: Date;
-    duration: number;
-    status: Status;
-    type: Type;
-    companyId: number;
-    userId?: number;
-    administratorId?: number;
+  id?: number;
+  pickupTime: Date;
+  duration: number;
+  status: Status;
+  type: Type;
+  companyId: number;
+  userId?: number;
+  administratorId?: number;
+  companyAdminName?: string;
 }
 
 export enum Status {
-    ON_HOLD = 0,
-    IN_PROGRESS = 1,
-    DONE = 2,
-    CANCELED = 3
+  ON_HOLD = 0,
+  IN_PROGRESS = 1,
+  DONE = 2,
+  CANCELED = 3,
 }
 
 export enum Type {
-    PREDEFINED = 0,
-    EXCEPTIONAL = 1
+  PREDEFINED = 0,
+  EXCEPTIONAL = 1,
 }
