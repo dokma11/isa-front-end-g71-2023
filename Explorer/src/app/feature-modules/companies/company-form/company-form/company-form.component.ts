4d0cc1fc232bc2b01d6291dab83ce8cc404dc4a7
dokma11 <vule.dok@gmail.com>
2023-12-17 18:58:12 +0100

    feat: Location choosing through map implementation

--- Explorer/src/app/feature-modules/companies/company-form/company-form/company-form.component.ts
+++ Explorer/src/app/feature-modules/companies/company-form/company-form/company-form.component.ts
@@ -2,6 +2,18 @@ import {Component, EventEmitter, Input, OnChanges, Output, ViewChild} from "@ang
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Company } from "../../model/company.model";
 import { CompaniesService } from "../../companies.service";
+import Map from 'ol/Map';
+import View from 'ol/View';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+import { Feature } from 'ol';
+import { Point } from 'ol/geom';
+import VectorSource from 'ol/source/Vector';
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: "xp-company-form",
@@ -21,6 +33,9 @@ export class CompanyFormComponent implements OnChanges {
   isAddButtonDisabled: boolean = true;
   isPublicChecked = false;
 
+  public map!: Map;
+  mapEnabled: boolean = false;
+
   ngOnChanges(): void {
       this.companyForm.reset();
       if (this.shouldEdit) {
@@ -28,8 +43,8 @@ export class CompanyFormComponent implements OnChanges {
               name: this.company.name || null,
               description: this.company.description || null,
               address: this.company.address || null,
-              longitude: this.company.longitude.toString() || null,
-              latitude: this.company.latitude.toString() || null,
+              longitude: this.company.longitude || null,
+              latitude: this.company.latitude || null,
               averageGrade: this.company.averageGrade.toString() || null
           };
           this.companyForm.patchValue(companyToPatch);
@@ -41,8 +56,8 @@ export class CompanyFormComponent implements OnChanges {
       description: new FormControl("", [Validators.required]),
       address: new FormControl("", [Validators.required]),
       averageGrade: new FormControl("", [Validators.required]),
-      longitude: new FormControl("", [Validators.required]),
-      latitude: new FormControl("", [Validators.required])
+      longitude: new FormControl(0.0, [Validators.required]),
+      latitude: new FormControl(0.0, [Validators.required])
   });
 
   addCompany(): void {
@@ -51,10 +66,8 @@ export class CompanyFormComponent implements OnChanges {
           description: this.companyForm.value.description || "",
           address: this.companyForm.value.address || "",
           averageGrade: parseInt(this.companyForm.value.averageGrade || "0") || 0,
-          longitude:
-              parseFloat(this.companyForm.value.longitude || "0") || 0,
-          latitude:
-              parseFloat(this.companyForm.value.latitude || "0") || 0,
+          longitude: this.companyForm.value.longitude || 0,
+          latitude: this.companyForm.value.latitude || 0,
       };
 
       if (this.newLatitude != 0 && this.newLongitude != 0) {
@@ -78,10 +91,8 @@ export class CompanyFormComponent implements OnChanges {
         description: this.companyForm.value.description || "",
         address: this.companyForm.value.address || "",
         averageGrade: parseInt(this.companyForm.value.averageGrade || "0") || 0,
-        longitude:
-            parseFloat(this.companyForm.value.longitude || "0") || 0,
-        latitude:
-            parseFloat(this.companyForm.value.latitude || "0") || 0,
+        longitude:this.companyForm.value.longitude || 0,
+        latitude: this.companyForm.value.latitude || 0,
       };
 
       company.id = this.company.id;
@@ -99,5 +110,62 @@ export class CompanyFormComponent implements OnChanges {
               location.reload();
           },
       });
+  }
+
+  enableMap(): void{
+    this.mapEnabled = true;
+
+    this.map = new Map({
+        target: 'hotel_map',
+        layers: [
+          new TileLayer({
+            source: new OSM()
+          })
+        ],
+        view: new View({
+          center: olProj.fromLonLat([19.831937147853964, 45.24572577637579]),
+          zoom: 14
+        })
+    });
+
+    this.map.on('click', (event: any) => {
+        const coordinate = event.coordinate;
+    
+        const lonLat = olProj.toLonLat(coordinate);
+    
+        console.log('Longitude:', lonLat[0]);
+        console.log('Latitude:', lonLat[1]);
+
+        this.companyForm.value.longitude = lonLat[0];
+        this.companyForm.value.latitude = lonLat[1];
+
+        this.map.getLayers().forEach((layer) => {
+            if (layer instanceof VectorLayer) {
+              this.map.removeLayer(layer);
+            }
+          });
+
+        const point = new Point(fromLonLat([lonLat[0], lonLat[1]]));
+
+        const startMarker = new Feature(point);
+
+        const markerStyle = new Style({
+              image: new Icon({
+                  anchor: [0.5, 1],
+                  src: 'http://www.pngall.com/wp-content/uploads/2017/05/Map-Marker-PNG-HD-180x180.png',
+                  scale: 0.4
+              })
+          });
+
+        startMarker.setStyle(markerStyle);
+
+        const vectorLayer = new VectorLayer({
+              source: new VectorSource({
+                  features: [startMarker]
+              })
+          });
+        
+        this.map.addLayer(vectorLayer);
+    });
   }
 }
