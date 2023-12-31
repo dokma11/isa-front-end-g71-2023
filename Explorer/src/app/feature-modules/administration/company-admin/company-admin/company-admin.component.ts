67c0991f22dab2be4a89f3291696858a053ae407
dokma11 <vule.dok@gmail.com>
2023-12-31 10:37:31 +0100

    style: Company admin profile style update

--- Explorer/src/app/feature-modules/administration/company-admin/company-admin/company-admin.component.ts
+++ Explorer/src/app/feature-modules/administration/company-admin/company-admin/company-admin.component.ts
@@ -3,6 +3,9 @@ import { CompanyAdmin, UserRole } from '../../model/company-admin.model';
 import { AdministrationService } from '../../administration.service';
 import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 import { User } from 'src/app/infrastructure/auth/model/user.model';
+import { CompanyAdminPasswordFormComponent } from '../../company-admin-password-form/company-admin-password-form.component';
+import { MatDialog } from '@angular/material/dialog';
+import { CompanyAdminFormComponent } from '../../company-admin-form/company-admin-form.component';
 
 @Component({
   selector: 'xp-company-admin',
@@ -28,9 +31,11 @@ export class CompanyAdminComponent implements OnInit{
   shouldEdit: boolean = false;
   shouldRenderCompanyAdminForm: boolean = false;
   shouldRenderCompanyAdminPasswordForm: boolean = false;
+  private dialogRef: any;
 
   constructor(private service: AdministrationService,
-              private authService: AuthService) { }
+              private authService: AuthService,
+              public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.authService.user$.subscribe(user => {
@@ -53,16 +58,14 @@ export class CompanyAdminComponent implements OnInit{
   }
 
   onEditClicked(companyAdmin: CompanyAdmin): void {
-    this.shouldEdit = true;
-    this.selectedCompanyAdmin = companyAdmin;
-    this.shouldRenderCompanyAdminForm = true;
-    this.shouldRenderCompanyAdminPasswordForm = false;
+    this.dialogRef = this.dialog.open(CompanyAdminFormComponent, {
+      data: companyAdmin,
+    });
   }
 
   onEditPasswordClicked(companyAdmin: CompanyAdmin): void {
-    this.shouldEdit = true;
-    this.selectedCompanyAdmin = companyAdmin;
-    this.shouldRenderCompanyAdminPasswordForm = true;
-    this.shouldRenderCompanyAdminForm = false;
+    this.dialogRef = this.dialog.open(CompanyAdminPasswordFormComponent, {
+      data: companyAdmin,
+    });
   }
 }
